115b20b21acd03526653454505ac54a48aa4227d
kenta <jackyutajack@gmail.com>
2023-08-11 15:56:05 +0900

    use custom action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kenta-ja8.github.io',
-  base: '/kenta-pages',
 	integrations: [mdx(), sitemap()],
 });
