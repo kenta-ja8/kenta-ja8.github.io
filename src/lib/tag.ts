4ce44dfa083dca198e26d30012c92decf7945904
kenta <jackyutajack@gmail.com>
2025-06-05 21:08:46 +0900

    add tag list

--- src/lib/tag.ts
+++ src/lib/tag.ts
@@ -1,7 +1,6 @@
 export function slugifyTag(name: string): string {
   return name
     .toLowerCase()
-    .replace(/\s+/g, '-')
+    .replace(/\s+/g, '')
     .replace(/_/g, '-')
-    .replace(/[^a-z0-9-]/g, '');
 }
